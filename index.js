71c5a45527db7bcfb6c6a1d6aaa66891cbe85cd7
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-04-25 16:38:33 -0600

    Intento por agregar el buscador

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const Children = require('./models/childrenModel');
 const Collection = require('./models/collectionModel');
+const Playlist = require('./models/playlistModel');
 const Sub = require('./models/subsModel');
+
 const cors = require('cors');
 
 const app = express();
@@ -43,6 +45,19 @@ const schema = buildSchema(`
   }
 `);
 
+// Ajusta tu esquema GraphQL
+const searchS = buildSchema(`
+  type Playlist {
+    _id: ID!
+    name: String!
+    url: String!
+  }
+
+  type Query {
+    searchVideos(childrenId: ID!, searchText: String!): [Playlist!]!
+  }
+`);
+
 // Definir los resolutores
 const root = {
   getChildCollections: async ({ childrenId }) => {
@@ -57,12 +72,40 @@ const root = {
   }
 };
 
-// Endpoint GraphQL
+const searchR = {
+  searchVideos: async ({ childrenId, searchText }) => {
+    try {
+      const subs = await Sub.find({ children: childrenId });
+      const collectionIds = subs.map(sub => sub.collection);
+      const collections = await Collection.find({ _id: { $in: collectionIds } });
+
+      // Filtrar los videos que coincidan con el searchText
+      const matchedVideos = collections.flatMap(collection =>
+        collection.videos.filter(video => video.name.includes(searchText))
+      );
+
+      return matchedVideos;
+    } catch (error) {
+      throw new Error('Error searching videos:', error);
+    }
+  }
+};
+
+
+// Endpoint GraphQL para consultas normales
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: true 
+  graphiql: true
 }));
+
+// Endpoint GraphQL para búsqueda de videos
+app.use('/graphql/search', graphqlHTTP({
+  schema: searchS,
+  rootValue: searchR,
+  graphiql: true
+}));
+
 
 // Iniciar el servidor
 app.listen(PORT, () => {
